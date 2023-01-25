3ea2ff2a46c8c370ed2bf910ed6cf19f45096a5a
Siba sankar mahunta <ssmahunta18@gmail.com>
2023-01-25 10:57:59 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import MaterialJsonForm from './components/material-json-form';
+
+function App() {
+  return (
+    <div className="App">
+      <h2 className='header'>Json Form Builder</h2>
+      <MaterialJsonForm/>
+    </div>
+  );
+}
+
+export default App;
